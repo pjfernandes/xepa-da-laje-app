9d70a862061d15083faa76175521f5cd9b790684
Pedro Fernandes <pjf_fernandes@yahoo.com.br>
2021-10-27 16:39:12 -0300

    product-new view added

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,7 +7,33 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.find(params[:id])
-    @user = User.find(@product.user_id)
+    #@user = User.find(@product.user_id)
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  # def create
+  #   @product = Product.new(product_params)
+  #   @product.user = current_user
+  #   @product.save
+  #   redirect_to root_path
+  # end
+
+  def create
+    @product = Product.new(product_params)
+    @product.user = current_user
+    if @product.save
+      redirect_to root_path, notice: 'Product was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:name, :unit_price, :quantity, :description)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   #root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "products/new", to: "products#new" # The `new` route
+  post "products", to: "products#create"
+
   root to: "products#index"
   get 'products/:id', to: 'products#show', as: :product
+
+
 
 end
