e7d09331b5724b8d63fcbaecd4d2c1a0b07ac54c
Pedro Fernandes <pjf_fernandes@yahoo.com.br>
2021-10-28 16:11:52 -0300

    navbar ok

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -1,12 +1,12 @@
 // app/javascript/components/banner.js
-import Typed from 'typed.js';
+// import Typed from 'typed.js';
 
-const loadDynamicBannerText = () => {
-  new Typed('#banner-typed-text', {
-    strings: ["Choose your construction trash!","aa"],
-    typeSpeed: 70,
-    loop: true
-  });
-}
+// const loadDynamicBannerText = () => {
+//   new Typed('#banner-typed-text', {
+//     strings: ["Choose your construction trash!","aa"],
+//     typeSpeed: 70,
+//     loop: true
+//   });
+// }
 
-export { loadDynamicBannerText };
+// export { loadDynamicBannerText };
