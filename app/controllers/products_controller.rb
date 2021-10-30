e40cabca6f0e847966ba01ee434794788d4b37ff
Pedro Fernandes <pjf_fernandes@yahoo.com.br>
2021-10-29 23:35:36 -0300

    info window added

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -25,7 +25,7 @@ class ProductsController < ApplicationController
         {
           lat: user.latitude,
           lng: user.longitude,
-          # info_window: render_to_string(partial: "info_window", locals: { user: user })
+          info_window: render_to_string(partial: "info_window", locals: { user: user })
         }
       end
   end
